3bb7ba0002fa1a5bda9a9435c36fd3b930ea29ea
simon-fu <simonok@139.com>
2023-06-24 19:55:25 +0800

    workflows: support env.REPO_GIT_HASH

--- nrelay/build.rs
+++ nrelay/build.rs
@@ -22,15 +22,31 @@ fn write_version() {
     let pkg_ver = std::env::var("CARGO_PKG_VERSION").unwrap();
     
 
-    let (hash_or_tag, _dirty) = built::util::get_repo_description(src.as_ref())
-    .with_context(||format!("src_path [{}], pkg_ver [{}]", src, pkg_ver))
-    .unwrap()
-    .with_context(||format!("src_path [{}], pkg_ver [{}]", src, pkg_ver))
-    .unwrap();
+    // let (hash_or_tag, _dirty) = built::util::get_repo_description(src.as_ref())
+    // .with_context(||format!("src_path [{}], pkg_ver [{}]", src, pkg_ver))
+    // .unwrap()
+    // .with_context(||format!("src_path [{}], pkg_ver [{}]", src, pkg_ver))
+    // .unwrap();
+
+    let hash_or_tag = get_git_hash(src.as_ref()).unwrap();
 
     let dst = format!("{}/out_version.txt", src);
     let mut built_file = std::fs::File::create(&dst).unwrap();
 
     // 1.0.8.8f01411
     built_file.write_all(format!("{}.{}",pkg_ver, hash_or_tag).as_bytes()).unwrap();
+}
+
+fn get_git_hash(src: &std::path::Path) -> anyhow::Result<String> {
+    let r = std::env::var("REPO_GIT_HASH");
+    if let Ok(h) = r {
+        if !h.is_empty() {
+            return Ok(h)
+        }
+    }
+
+    let (hash_or_tag, _dirty) = built::util::get_repo_description(src.as_ref())
+    .with_context(||"get_repo_description fail")?
+    .with_context(||"empty repo desc")?;
+    Ok(hash_or_tag)
 }
